0e3f9dca625fe96856aaba6e6fcb3f59e63a56b0
Nishant Gautam <eddienishant@gmail.com>
2023-01-02 08:50:38 -0500

    Assignment 02- Igniting our app

--- Coding_Assignments/App.js
+++ Coding_Assignments/App.js
@@ -1,15 +1,21 @@
 // Render an <h1 class="namaste">Namaste React using React</h1> on the page inside the given root.
 
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+
+
 
 // Rendering in the root
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const root = document.querySelector("#root");
+
 const reactElement = React.createElement("h1", {className: "namaste"}, "Namaste React using React");
-root.render(reactElement)
+createRoot(root).render(reactElement)
 
 
 // Nested React Elements 
-const paragraphElement = React.createElement("p", {className:"text"},  "Chapter 01-Inception");
+const paragraphElement = React.createElement("p", {className:"text"},  "Chapter 02-Igniting our App");
 const container = React.createElement("div", {className:"container"}, [reactElement, paragraphElement])
-root.render(container);
+createRoot(root).render(container);
 
 
